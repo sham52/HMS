582212f697d0240233817d7a50b194c67dfedd65
Shamil <122603654+sham52@users.noreply.github.com>
2024-03-17 01:44:38 +0300

    some backend changes

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const db = require('./db');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
