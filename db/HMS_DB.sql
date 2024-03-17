c980e79c2cc350b91bb06eb7bf5479e74205e906
Shamil <122603654+sham52@users.noreply.github.com>
2024-03-17 03:28:07 +0300

    DB updated

--- db/HMS_DB.sql
+++ db/HMS_DB.sql
@@ -1,57 +1,62 @@
 -- DROP DATABASE IF EXISTS hms;
 -- CREATE DATABASE hms;
 USE hms;
+
 CREATE TABLE Patients (
-	patientID VARCHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    patientID VARCHAR(36) PRIMARY KEY DEFAULT (UUID()),
     firstName VARCHAR(255),
     lastName VARCHAR(255),
     dateOfBirth DATE,
     gender ENUM('Erkek', 'Kadın', 'Diğer'),
     address VARCHAR(255),
-	phoneNumber VARCHAR(255),
+    phoneNumber VARCHAR(255),
     email VARCHAR(255)
 );
-CREATE TABLE Departments(
-   DepartmentID INT AUTO_INCREMENT PRIMARY KEY,
-   DepartmentName VARCHAR(255)
+
+CREATE TABLE Departments (
+    departmentID INT AUTO_INCREMENT PRIMARY KEY,
+    departmentName VARCHAR(255)
 );
+
 CREATE TABLE Doctors (
-   DoctorID INT AUTO_INCREMENT PRIMARY KEY,
-   FirstName VARCHAR(255),
-   LastName VARCHAR(255),
-   Gender ENUM('Erkek', 'Kadın', 'Diğer'),
-   DepartmentID INT,
-   FOREIGN KEY (DepartmentID) REFERENCES Departments(DepartmentID)
+    doctorID INT AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(255),
+    lastName VARCHAR(255),
+    gender ENUM('Erkek', 'Kadın', 'Diğer'),
+    departmentID INT,
+    FOREIGN KEY (departmentID) REFERENCES Departments(departmentID)
 );
+
 CREATE TABLE Appointments (
-   AppointmentID VARCHAR(36) PRIMARY KEY DEFAULT (UUID()),
-   PatientID VARCHAR(36),
-   DoctorID INT,
-   AppointmentDate DATE,
-   FOREIGN KEY (PatientID) REFERENCES Patients(PatientID),
-   FOREIGN KEY (DoctorID) REFERENCES Doctors(DoctorID)
+    appointmentID VARCHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    patientID VARCHAR(36),
+    doctorID INT,
+    appointmentDate DATE,
+    FOREIGN KEY (patientID) REFERENCES Patients(patientID),
+    FOREIGN KEY (doctorID) REFERENCES Doctors(doctorID)
 );
+
 CREATE TABLE Prescriptions (
-    PrescriptionID VARCHAR(36) PRIMARY KEY DEFAULT (UUID()),
-    PatientID VARCHAR(36),
-    DoctorID INT,
-    PrescriptionDate DATE,
-    Medication VARCHAR(255),
-    FOREIGN KEY (PatientID) REFERENCES Patients(PatientID),
-    FOREIGN KEY (DoctorID) REFERENCES Doctors(DoctorID)
+    prescriptionID VARCHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    patientID VARCHAR(36),
+    doctorID INT,
+    prescriptionDate DATE,
+    medication VARCHAR(255),
+    FOREIGN KEY (patientID) REFERENCES Patients(patientID),
+    FOREIGN KEY (doctorID) REFERENCES Doctors(doctorID)
 );
+
 CREATE TABLE PatientDoctor (
-    VisitID INT AUTO_INCREMENT PRIMARY KEY,
-	PatientID VARCHAR(36),  
-    DoctorID INT,
-    VisitDate DATE,
-    UNIQUE KEY (PatientID, DoctorID),
-    FOREIGN KEY (PatientID) REFERENCES Patients(PatientID),
-    FOREIGN KEY (DoctorID) REFERENCES Doctors(DoctorID)
+    visitID INT AUTO_INCREMENT PRIMARY KEY,
+    patientID VARCHAR(36),
+    doctorID INT,
+    departmentID INT,  -- Added departmentID to track department of the visit
+    visitDate DATE,
+    UNIQUE KEY (patientID, doctorID),
+    FOREIGN KEY (patientID) REFERENCES Patients(patientID),
+    FOREIGN KEY (doctorID) REFERENCES Doctors(doctorID),
+    FOREIGN KEY (departmentID) REFERENCES Departments(departmentID)  -- Added foreign key constraint
 );
-ALTER TABLE Appointments
-ADD CONSTRAINT fk_doctor_appointment FOREIGN KEY (DoctorID) REFERENCES Doctors(DoctorID),
-ADD CONSTRAINT fk_patient_appointment FOREIGN KEY (PatientID) REFERENCES Patients(PatientID);
 
 -- Insert patients
 INSERT INTO Patients (patientID, firstName, lastName, dateOfBirth, gender, address, phoneNumber, email)
@@ -65,7 +70,6 @@ INSERT INTO Departments (departmentID, departmentName)
 VALUES 
   (1, 'Dahiliye'),
   (2, 'Cerrahi');
-  
 
 -- Insert Turkish doctors
 INSERT INTO Doctors (firstName, lastName, gender, departmentID)
@@ -74,9 +78,8 @@ VALUES
   ('Ali', 'Çelik', 'Erkek', 2),
   ('Zeynep', 'Türk', 'Kadın', 1);
 
-
--- INSERT INTO PatientDoctor (PatientID, DoctorID, VisitDate)
--- VALUES ('a11450f5-dcd5-11ee-a372-4ccc6a43e5c9', '2', '2024-03-07');
+-- INSERT INTO PatientDoctor (PatientID, DoctorID, DepartmentID, VisitDate)
+-- VALUES ('a11450f5-dcd5-11ee-a372-4ccc6a43e5c9', '2', '1', '2024-03-07');
 
 SELECT * FROM Patients;
 SELECT * FROM Doctors;
@@ -84,8 +87,3 @@ SELECT * FROM Departments;
 SELECT * FROM Appointments;
 SELECT * FROM Prescriptions;
 SELECT * FROM PatientDoctor;
-
-
-
-
-
