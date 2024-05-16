91ed514e7189301534a492346cd105f6f7acc948
Shamil <122603654+sham52@users.noreply.github.com>
2024-05-16 21:34:02 +0300

    auth bug fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
   useEffect(() => {
     const authTokenFromCookie = getAuthTokenFromCookie();
     if (!authTokenFromCookie) {
-      navigate("/login");
+      return;
     } else {
       setAuthToken(authTokenFromCookie);
     }
