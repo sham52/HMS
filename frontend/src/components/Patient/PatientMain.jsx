65cfd2dbf9fb5856af8e49a71aeda5b0149d38b8
Shamil <122603654+sham52@users.noreply.github.com>
2024-05-17 10:47:38 +0300

    olcak bişiler

--- frontend/src/components/Patient/PatientMain.jsx
+++ frontend/src/components/Patient/PatientMain.jsx
@@ -10,47 +10,66 @@ import {
   Td,
   Flex,
   TableContainer,
+  Text,
+  Avatar,
+  Center,
 } from "@chakra-ui/react";
 import { useAuth } from "../../context/AuthContext";
 import axios from "axios";
 
 const PatientMain = () => {
-  const [patientData, setPatientData] = useState(null);
+  const [patientData, setPatientData] = useState({
+    patientID: null,
+    fullName: null,
+    userType: null,
+    dateOfBirth: null,
+    gender: null,
+    phoneNumber: null,
+    email: null,
+    medications: null,
+    appointments: [
+      {
+        appointmentDate: null,
+        doctorName: null,
+        departmentName: null,
+      },
+    ],
+  });
 
   useEffect(() => {
     const fetchPatientData = async () => {
       try {
-        // Get patientId from cookies
-        const patientId = localStorage.getItem("patientId");
-
-        const response = await axios.get(`/patients/${patientId}`);
+        await setPatientData({
+          ...patientData,
+          patientID: localStorage.getItem("userID"),
+          userType: localStorage.getItem("userType"),
+          fullName: localStorage.getItem("fullName"),
+        });
+        const response = await axios.get(
+          `/patients/${localStorage.getItem("userID")}`
+        );
         setPatientData(response.data);
       } catch (err) {
         console.error("Error fetching patient:", err);
       }
     };
     fetchPatientData();
-  }, []);
-
-  const appointments = [
-    {
-      appointmentDate: "2021-10-10",
-      doctor: {
-        firstName: "John",
-        lastName: "Doe",
-      },
-    },
-  ];
-
-  const { authToken, setAuthToken } = useAuth();
-  console.log(authToken);
-  console.log("bu KOMPONENTN ÇALIŞIYOR LA GARDAŞ");
+  }, [patientData.patientID]);
 
   return (
     <>
       <Flex justifyContent="center" alignItems="center" height="100vh">
         <Box mt={16} width="80%">
-          <TableContainer>
+          <Center>
+            <Box p="6" borderWidth="1px" borderRadius="lg">
+              <Avatar size="xl" name={patientData.fullName} />
+              <Text mt={4} fontSize="xl" fontWeight="bold">
+                {patientData.fullName}
+              </Text>
+              <Text mt={2}>{patientData.userType}</Text>
+            </Box>
+          </Center>
+          <TableContainer mt={8}>
             <Table variant="striped" colorScheme="teal">
               <TableCaption>Patient Appointments</TableCaption>
               <Thead>
@@ -61,10 +80,10 @@ const PatientMain = () => {
                 </Tr>
               </Thead>
               <Tbody>
-                {appointments.map((appointment, index) => (
-                  <Tr key={index}>
+                {patientData.appointments?.map((appointment) => (
+                  <Tr key={appointment.appointmentDate}>
                     <Td>{appointment.appointmentDate}</Td>
-                    <Td>{`${appointment.doctor.firstName} ${appointment.doctor.lastName}`}</Td>
+                    <Td>{`${appointment.doctorName}`}</Td>
                     <Td>{appointment.medicationDetails || "N/A"}</Td>
                   </Tr>
                 ))}
